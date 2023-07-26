d719ef24d4a13fb5122dbea69ac49ea0231b8e01
Hanan Munir <hananmunir471gmail.com>
2023-07-27 02:39:56 +0500

    Fixed logo

--- src/Components/ModelBlue.jsx
+++ src/Components/ModelBlue.jsx
@@ -36,17 +36,6 @@ export function ModelBlue(props) {
   const [color, setColor] = useState("#e7e9ed");
   const groupRef = useRef();
 
-  // const { rotation, position } = useControls({
-  //   rotation: {
-  //     value: [0, 0, 0],
-  //     step: 0.01,
-  //   },
-  //   position: {
-  //     value: [0, 0.2, 20],
-  //     step: 0.01,
-  //   },
-  // });
-
   useEffect(() => {
     props.setModelLoaded(true);
     // Animation timeline using gsap
@@ -54,7 +43,7 @@ export function ModelBlue(props) {
 
     // Initial spinning animation with movement
     timeline.to(groupRef.current.position, {
-      x: window.innerWidth < 440  ? -0.4 : 0,
+      x: window.innerWidth < 440 ? -0.4 : 0,
       z: -10.5,
       duration: 2,
       ease: "power1.inOut",
@@ -74,7 +63,6 @@ export function ModelBlue(props) {
         z: -8,
         duration: 1,
         onStart: () => {
-          //{"rotation":[-0.8400000000000005,0.6000000000000004,0.6200000000000004]}
           gsap.to(groupRef.current.rotation, {
             x: -0.84,
             y: 0.6,
@@ -91,16 +79,24 @@ export function ModelBlue(props) {
         duration: 1,
         onStart: () => {
           gsap.to(groupRef.current.rotation, {
-            x: window.innerWidth < 440  ? -0.4 : 0,
+            x: window.innerWidth < 440 ? -0.4 : 0,
             y: -0.64,
             z: 0,
             duration: 1,
           });
         },
       });
+    } else if (activeState === 1) {
+      gsap.to(groupRef.current.rotation, {
+        y: Math.PI,
+        x: 0,
+        z: 0,
+
+        duration: 1,
+      });
     } else if (activeState === 0) {
       gsap.to(groupRef.current.position, {
-        x: window.innerWidth < 440  ? -0.4 : 0,
+        x: window.innerWidth < 440 ? -0.4 : 0,
         y: 0.2,
         z: -10.5,
         duration: 1,
@@ -123,11 +119,14 @@ export function ModelBlue(props) {
   return (
     <group>
       {activeState === 1 && (
-        <Html position={[window.innerWidth < 440  ? -0.3 : 0, 0, -9]}>
+        <Html position={[window.innerWidth < 440 ? -0.3 : 0, 0, -9]}>
           <div className='color-container'>
-            <span className='color-header' style={{
-              top: window.innerWidth < 440  ? '77%' : '80%',
-            }}>
+            <span
+              className='color-header'
+              style={{
+                top: window.innerWidth < 440 ? "77%" : "80%",
+              }}
+            >
               {colorArray.filter((item) => item.hex === color)[0].name}
             </span>
 
@@ -155,14 +154,9 @@ export function ModelBlue(props) {
           </div>
         </Html>
       )}
-      <group
-        ref={groupRef}
-        scale={24}
-        rotation={[0, 0, 0]}
-        {...props}
-        dispose={null}
-      >
-        <PresentationControls enabled={activeState !== 3}>
+
+      <group ref={groupRef} scale={24} {...props} dispose={null}>
+        <PresentationControls>
           <mesh
             geometry={nodes["1-11-05815F_8002"].geometry}
             material={materials.WHITESMOKE}
@@ -175,12 +169,18 @@ export function ModelBlue(props) {
             geometry={nodes.Circle002.geometry}
             material={materials.Flash}
           />
-          <mesh
-            material-color={activeState === 1 ? color : "#e7e9ed"}
-            geometry={nodes["GLASS-X19-BATT-COVER002"].geometry}
-            material={materials["Glass.002"]}
-            rotation={[Math.PI / 2, 0, 0]}
-          />
+          <group rotation={[Math.PI / 2, 0, 0]}>
+            <mesh
+              geometry={nodes["GLASS-X19-BATT-COVER002_1"].geometry}
+              material={materials["Glass.002"]}
+              material-color={activeState === 1 ? color : "#e7e9ed"}
+            />
+            <mesh
+              geometry={nodes["GLASS-X19-BATT-COVER002_2"].geometry}
+              material={materials["Glass.002"]}
+              material-color={activeState === 1 ? color : "#e7e9ed"}
+            />
+          </group>
           <mesh
             geometry={nodes["QT-X19-2M-CAM-LENS003"].geometry}
             material={materials["transp Glass"]}
@@ -188,6 +188,10 @@ export function ModelBlue(props) {
           <mesh
             geometry={nodes["QT-X19-BATT-COVER-PRINTING-INK002"].geometry}
             material={materials.Material}
+          />
+          <mesh
+            geometry={nodes["QT-X19-MAIN-CAM-LENS003"].geometry}
+            material={materials["transp Glass"]}
           />
           <mesh
             geometry={nodes["QT-X19-MAIN-MIC-MESH002"].geometry}
@@ -198,13 +202,17 @@ export function ModelBlue(props) {
             material={materials.mesh}
           />
           <mesh
+            geometry={nodes["QT-X19-SUB-CAM-LENS003"].geometry}
+            material={materials["transp Glass"]}
+          />
+          <mesh
             geometry={nodes["SJ-X19-BOT-COVER002"].geometry}
             material={materials.PHONE_COLOR_1}
             material-color={activeState === 1 ? color : "#e7e9ed"}
           />
           <mesh
-            geometry={nodes["SJ-X19A-CAM-DECO-50M002"].geometry}
-            material={materials.Chrome}
+            geometry={nodes["SJ-X19-DECO-RING-2009"].geometry}
+            material={materials.Camera_Black}
           />
           <mesh
             geometry={nodes.SOLID039.geometry}
@@ -213,9 +221,16 @@ export function ModelBlue(props) {
           <mesh
             geometry={nodes.redmi.geometry}
             material={materials.labels}
-            position={[0.022, -0.059, -0.003]}
+            position={[0.022, -0.059, -0.0035]}
             rotation={[-Math.PI / 2, -Math.PI / 2, 0]}
             scale={0.163}
+          />
+          <mesh
+            geometry={nodes.Circle.geometry}
+            material={materials.labels}
+            position={[0.007, 0.045, -0.003]}
+            rotation={[Math.PI / 2, 0, 0]}
+            scale={0.002}
           />
           <mesh
             geometry={nodes["02_GLASS_X19_TP002_1"].geometry}
@@ -238,6 +253,30 @@ export function ModelBlue(props) {
             material={materials.Camera_Black_Metallic}
           />
           <mesh
+            geometry={nodes.Lens_1008_1.geometry}
+            material={materials.Lens}
+          />
+          <mesh
+            geometry={nodes.Lens_1008_2.geometry}
+            material={materials.Camera_Black_Metallic}
+          />
+          <mesh
+            geometry={nodes.Lens_1009_1.geometry}
+            material={materials.Lens}
+          />
+          <mesh
+            geometry={nodes.Lens_1009_2.geometry}
+            material={materials.Camera_Black_Metallic}
+          />
+          <mesh
+            geometry={nodes.Lens_1010_1.geometry}
+            material={materials.Lens}
+          />
+          <mesh
+            geometry={nodes.Lens_1010_2.geometry}
+            material={materials.Camera_Black_Metallic}
+          />
+          <mesh
             geometry={nodes["SJ-X19-DECO-RING-2007_1"].geometry}
             material={materials.Black_Glossy}
           />
@@ -250,6 +289,18 @@ export function ModelBlue(props) {
             material={materials.Camera_Black_Glossy}
           />
           <mesh
+            geometry={nodes["SJ-X19-DECO-RING-2008_1"].geometry}
+            material={materials.Black_Glossy}
+          />
+          <mesh
+            geometry={nodes["SJ-X19-DECO-RING-2008_2"].geometry}
+            material={materials.Camera_Black}
+          />
+          <mesh
+            geometry={nodes["SJ-X19-DECO-RING-2008_3"].geometry}
+            material={materials.Camera_Black_Glossy}
+          />
+          <mesh
             geometry={nodes["SJ-X19-DECO-RING-3002_1"].geometry}
             material={materials.Black_Glossy}
           />
@@ -259,6 +310,14 @@ export function ModelBlue(props) {
           />
           <mesh
             geometry={nodes["SJ-X19-DECO-RING-3002_3"].geometry}
+            material={materials.Camera_Black_Glossy}
+          />
+          <mesh
+            geometry={nodes["SJ-X19A-CAM-DECO-50M002_1"].geometry}
+            material={materials.Chrome}
+          />
+          <mesh
+            geometry={nodes["SJ-X19A-CAM-DECO-50M002_2"].geometry}
             material={materials.Camera_Black_Glossy}
           />
           <mesh
